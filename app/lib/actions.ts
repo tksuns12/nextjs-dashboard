141907a42e40832868a0eb7f0c76de30d7aac89a
tksuns12 <tksuns@gmail.com>
2024-03-04 21:29:45 +0900

    chap14 added server-side validation for edit-invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,25 +62,44 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: Number(formData.get('amount')),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: Number(formData.get('amount')),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
   try {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: Number(formData.get('amount')),
-      status: formData.get('status'),
-    });
-
-    const amountInCents = amount * 100;
-
     await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
     `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice',
+    };
+  }
 
-    revalidatePath('/dashboard/invoices');
-  } catch (error) {}
-
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
